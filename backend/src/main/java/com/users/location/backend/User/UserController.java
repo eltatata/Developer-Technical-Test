0ba8697f48a0c225657c048f899eb80c895b40d7
eltatataa <davidtowo75@gmail.com>
2024-08-19 23:11:47 -0500

    backend: user controller

--- backend/src/main/java/com/users/location/backend/User/UserController.java
+++ backend/src/main/java/com/users/location/backend/User/UserController.java
@@ -1,4 +1,52 @@
 package com.users.location.backend.User;
 
+import com.users.location.backend.User.DTO.UserDTO;
+import com.users.location.backend.User.Entities.User;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping
+@Validated
 public class UserController {
+  @Autowired
+  UserService userService;
+
+  @GetMapping
+  public ResponseEntity<List<User>> getAllUsers() {
+    List<User> users = userService.getAllUsers();
+    return ResponseEntity.ok(users);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<User> getUserById(@PathVariable Long id) {
+    Optional<User> user = userService.getUserById(id);
+    return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public ResponseEntity<User> createUser(@Valid @RequestBody UserDTO userDTO) {
+    User newUser = userService.createUser(userDTO);
+    return ResponseEntity.ok(newUser);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<User> updateUser(@PathVariable Long id, @Valid @RequestBody UserDTO userDTO) {
+      User updatedUser = userService.updateUser(id, userDTO);
+      return ResponseEntity.ok(updatedUser);
+  }
+
+  @DeleteMapping("/{id}")
+  public  ResponseEntity<User> deleteUser(@PathVariable Long id) {
+    userService.deleteUser(id);
+    return ResponseEntity.noContent().build();
+  }
 }
