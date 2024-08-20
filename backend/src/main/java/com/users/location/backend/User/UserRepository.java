6da53fcf2770b8853c341f5a741126c1e00a4d25
eltatataa <davidtowo75@gmail.com>
2024-08-19 22:09:16 -0500

    backend: user repository

--- backend/src/main/java/com/users/location/backend/User/UserRepository.java
+++ backend/src/main/java/com/users/location/backend/User/UserRepository.java
@@ -1,4 +1,7 @@
 package com.users.location.backend.User;
 
-public class UserRepository {
+import com.users.location.backend.User.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
