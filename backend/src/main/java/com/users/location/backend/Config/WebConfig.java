ef5b51f08309a9211126877a96bae4a48de2bd87
eltatataa <davidtowo75@gmail.com>
2024-08-21 08:17:41 -0500

    backend: cors config

---
+++ backend/src/main/java/com/users/location/backend/Config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.users.location.backend.Config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+  @Value("${frontend.url}")
+  private String frontendUrl;
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins(frontendUrl)
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+  }
+}
