10c284623a320ca41cea82b5a67f5073cf1d3c8d
eltatataa <davidtowo75@gmail.com>
2024-08-20 23:40:17 -0500

    frontend: added map

--- frontend/src/app/users/[id]/page.tsx
+++ frontend/src/app/users/[id]/page.tsx
@@ -1,9 +1,12 @@
+import dynamic from 'next/dynamic'
 import UserForm from '@/components/UserForm'
+const Map = dynamic(() => import('@/components/Map'), { ssr: false })
 
 export default function SingleUserPage() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-7xl mx-auto gap-20 p-6">
       <UserForm />
+      <Map />
     </div>
   )
 }
---
+++ frontend/src/components/Map.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import L from 'leaflet'
+import MarkerIcon from '../../node_modules/leaflet/dist/images/marker-icon.png'
+import MarkerShadow from '../../node_modules/leaflet/dist/images/marker-shadow.png'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+export default function Map() {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      className='rounded-lg'
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker icon={
+        new L.Icon({
+          iconUrl: MarkerIcon.src,
+          iconRetinaUrl: MarkerIcon.src,
+          iconSize: [25, 41],
+          iconAnchor: [12.5, 41],
+          popupAnchor: [0, -41],
+          shadowUrl: MarkerShadow.src,
+          shadowSize: [41, 41],
+        })
+      } position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  )
+}
